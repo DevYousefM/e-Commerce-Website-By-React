7f2ac0df769deed568239be65db22a23e23e088f
cap_Yousef <joseph01145119185@gmail.com>
2022-03-15 13:21:30 +0200

    Upload Design

---
+++ build/precache-manifest.63d65dd674d28935078d11503fe71f36.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53cdba590990d24a5e9f18f15f774bc6",
+    "url": "/e-Commerce-Website-By-React/index.html"
+  },
+  {
+    "revision": "bec3e799bff12e5f3680",
+    "url": "/e-Commerce-Website-By-React/static/css/2.acc90196.chunk.css"
+  },
+  {
+    "revision": "06aaa04d9fa7f557ba30",
+    "url": "/e-Commerce-Website-By-React/static/css/main.bda5db52.chunk.css"
+  },
+  {
+    "revision": "bec3e799bff12e5f3680",
+    "url": "/e-Commerce-Website-By-React/static/js/2.58889479.chunk.js"
+  },
+  {
+    "revision": "bb7cd3a64b6907d075891e5c2f9c42e7",
+    "url": "/e-Commerce-Website-By-React/static/js/2.58889479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06aaa04d9fa7f557ba30",
+    "url": "/e-Commerce-Website-By-React/static/js/main.f061a6f6.chunk.js"
+  },
+  {
+    "revision": "c6d8916c049d2e796327",
+    "url": "/e-Commerce-Website-By-React/static/js/runtime-main.2f4f5921.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/e-Commerce-Website-By-React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/e-Commerce-Website-By-React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/e-Commerce-Website-By-React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/e-Commerce-Website-By-React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/e-Commerce-Website-By-React/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
